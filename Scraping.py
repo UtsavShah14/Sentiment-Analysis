93b98aab64e6ca32f04542c1b29d117ade95a7a3
Utsav Shah <UtsavShah14@users.noreply.github.com>
2020-08-05 20:50:04 +0100

    Integrating with Flask Added two html files
    Signed-off-by: Utsav Shah <UtsavShah14@users.noreply.github.com>

--- Scraping.py
+++ Scraping.py
@@ -1,43 +1,85 @@
 # import os
+import flask
 import pandas as pd
 import tweepy as tw
 
 import Access_Keys
 
-start_url = ""
+app = flask.Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def landingpage():
+    return flask.render_template('index.html')
+
+
+@app.route('/login')
+def helloworld():
+    print('Hello')
+    # ts = TwitterStreamer()
+    # api = ts.authenticate()
+    search_words = input("Enter a topic that you are looking for") + " -filter:retweets"
+    # date_since = input("Enter a date")
+    # return search_words, date_since
+    return search_words
+
+
 consumer_API_key = Access_Keys.consumer_API_key
 consumer_API_secret_key = Access_Keys.consumer_API_secret_key
 access_token = Access_Keys.access_token
 access_token_secret = Access_Keys.access_token_secret
 
-auth = tw.OAuthHandler(consumer_API_key, consumer_API_secret_key)
-auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
 
-# api.update_status("Look, I'm tweeting from #Python in my #BigData Thesis!")
+class TwitterStreamer:
 
-search_words = "#wildfires"
-date_since = "2018-11-16"
-#
-# tweets = tw.Cursor(api.search,
-#               q=search_words,
-#               lang="en",
-#               since=date_since).items(5)
-# tweets
-#
-tweets = tw.Cursor(api.search,
-                   q=search_words,
-                   lang="en",
-                   since=date_since).items(5)
+    def __init__(self):
+        api = self.authenticate()
 
-# print([tweet.text for tweet in tweets])
+    def authenticate(self):
+        auth = tw.OAuthHandler(consumer_API_key, consumer_API_secret_key)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tw.API(auth, wait_on_rate_limit=True)
+        return api
 
-users_locs = [[tweet.text, tweet.user.screen_name, tweet.user.location] for tweet in tweets]
-# users_locs
+    def get_tweet(self, search_words, date_since, api):
+        tweets = tw.Cursor(api.search,
+                           q=search_words,
+                           lang="en",
+                           since=date_since).items(50)
+        return tweets
 
-pd.set_option('display.max_columns', None)
-tweet_text = pd.DataFrame(data=users_locs,
-                          columns=['tweets', 'user', "location"])
-print(tweet_text)
+    def display_tweets(self, tweets):
+        pd.set_option('display.max_columns', None)
+        data = [[tweet.text, tweet.user.screen_name, tweet.user.location] for tweet in tweets]
+        tweet_text = pd.DataFrame(data=data, columns=['tweets', 'user', "location"])
+        print(tweet_text)
+        return tweet_text
 
-tweet_text.to_csv("Tweets.csv", index=False, header=True)
+    def listener(self):
+        pass
+
+    def get_home_timeline(self):
+        statuses = api.home_timeline()
+        for i, status in enumerate(statuses):
+            print(str(i) + ": " + status.text)
+
+    def get_trends(self):
+        trends = api.trends_place(2352824)
+        for trend in trends[0]["trends"][:10]:
+            print(trend["name"])
+
+
+if __name__ == '__main__':
+    # ts = TwitterStreamer()
+    # api = ts.authenticate()
+    # search_words = input("Enter a topic that you are looking for") + " -filter:retweets"
+    # date_since = input("Enter a date")
+    # tweets = ts.get_tweet(search_words, date_since, api)
+    # tweet_text = ts.display_tweets(tweets)
+    # tweet_text.to_csv("Tweets.csv", index=False, header=True)
+    # ts.get_home_timeline()
+    # trends = api.trends_available
+    # for trend in trends:
+    #     print(trend)
+    app.run(debug=True)
