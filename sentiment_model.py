5d7c2335793f387af9e236aafeead6f0a188bb53
Utsav Shah <UtsavShah14@users.noreply.github.com>
2020-08-25 13:51:28 +0100

    Added Logistic Regression Model and simplified the code. Cleaned.
    Signed-off-by: Utsav Shah <UtsavShah14@users.noreply.github.com>

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
@@ -13,81 +14,82 @@ from preprocessing_data import basic_stopwords_list
 
 class TrainingModel:
     def __init__(self):
-        pass
+        tweet_csv = pd.read_csv("Tweets.csv")
+        self.text_list = []
+        self.text_sentiment_list = []
+        for i, index in enumerate(tweet_csv.index):
+            self.text_list.append(tweet_csv['Text'][index])
+            self.text_sentiment_list.append(tweet_csv['Sentiment'][index])
 
-    def svm_classifier(self):
-        pass
+    def preprocessor(self):
+        ngram_vectorizer = CountVectorizer(
+            binary=True,
+            ngram_range=(1, 3),
+            max_features=500,
+            stop_words=basic_stopwords_list
+        )
 
-    def logr_classifier(self):
-        pass
+        corpus = ngram_vectorizer.fit_transform(self.text_list)
+
+        x_train, x_val, y_train, y_val = train_test_split(
+            corpus, train.text_sentiment_list, train_size=0.8
+        )
+        return x_train, x_val, y_train, y_val
+
+    @staticmethod
+    def svm_classifier(x_train, x_val, y_train, y_val):
+        svm = LinearSVC(C=0.05)
+        svm.fit(x_train, y_train)
+        prediction = svm.predict(x_val)
+        print("Accuracy for SVM: " + str(accuracy_score(y_val, prediction)))
+
+    @staticmethod
+    def logr_classifier(x_train, x_val, y_train, y_val):
+        log_model = LogisticRegression(C=0.5, max_iter=250)
+        log_model.fit(x_train, y_train)
+        prediction = log_model.predict(x_val)
+        print("Accuracy for LR: " + str(accuracy_score(y_val, prediction)))
 
 
 if __name__ == '__main__':
-    tweet_csv = pd.read_csv("Tweets.csv")
-    text_list = []
-    text_sentiment_list = []
-    for i, index in enumerate(tweet_csv.index):
-        text_list.append(tweet_csv['Text'][index])
-        text_sentiment_list.append(tweet_csv['Sentiment'][index])
-    ngram_vectorizer = CountVectorizer(
-        binary=True,
-        ngram_range=(1, 3),
-        max_features=500,
-        stop_words=basic_stopwords_list
-    )
-    print(ngram_vectorizer.stop_words)
-    X = ngram_vectorizer.fit_transform(text_list)
-    # X = ngram_vectorizer.transform(text_list)
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, text_sentiment_list, train_size=0.8
-    )
-    heloo = ngram_vectorizer.transform(['Hi whatsapp'])
-    svm = LinearSVC(C=0.5)
-    svm.fit(X_train, y_train)
-    prediction = svm.predict(X_val)
-    # print(prediction)
-    print("Accuracy for C=0.5: %s" % (accuracy_score(y_val, prediction)))
-    print(svm.predict(heloo))
+    train = TrainingModel()
+    x_training, x_validation, y_training, y_validation = train.preprocessor()
+    train.svm_classifier(x_training, x_validation, y_training, y_validation)
+    train.logr_classifier(x_training, x_validation, y_training, y_validation)
 
-# ngram_vectorizer = CountVectorizer(
-#     binary=True,
-#     ngram_range=(1, 2)
-# )
-# print("Fit-vectorize")
-# ngram_vectorizer.fit(corpus_clean)
-# print("Transform-vectorize")
-# x = ngram_vectorizer.transform(corpus_clean)
-# # # x_x_test =
-# # # features = vectorizer.fit_transform(text_list)
-# # # print((vectorizer.get_feature_names()))
-# # # features_nd = features.toarray(dtype=object)  # for easy usage
-# #
-# X_train, X_test, y_train, y_test = train_test_split(
-#     x,
-#     text_sentiment_list,
-#     train_size=0.80,
-# )
-# # # random_state=1234
+    # tweet_csv = pd.read_csv("Tweets.csv")
+    # text_list = []
+    # text_sentiment_list = []
+    # for i, index in enumerate(tweet_csv.index):
+    #     text_list.append(tweet_csv['Text'][index])
+    #     text_sentiment_list.append(tweet_csv['Sentiment'][index])
+
+    # ngram_vectorizer = CountVectorizer(
+    #     binary=True,
+    #     ngram_range=(1, 3),
+    #     max_features=500,
+    #     stop_words=basic_stopwords_list
+    # )
+    #
+    # X = ngram_vectorizer.fit_transform(train.text_list)
+    #
+    # X_train, X_val, y_train, y_val = train_test_split(
+    #     X, train.text_sentiment_list, train_size=0.8
+    # )
+
+    # svm = LinearSVC(C=0.05)
+    # svm.fit(X_train, y_train)
+    # prediction = svm.predict(X_val)
+    # print(prediction)
+    # print("Accuracy: " + str(accuracy_score(y_val, prediction)))
+    # print(svm.predict(heloo))
+
 # for c in [0.01, 0.05, 0.25, 0.5, 1]:
 #     log_model = LogisticRegression(C=c, max_iter=250)
 #     log_model = log_model.fit(X=X_train, y=y_train)
 #     y_pred = log_model.predict(X_test)
 #     print("Accuracy for C=%s: %s" % (c, accuracy_score(y_test, y_pred)))
 # # # print(accuracy_score(y_test, y_pred))
-
-# ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3), stop_words=basic_stopwords_list)
-# X = ngram_vectorizer.fit_transform(text_list)
-# X = ngram_vectorizer.transform(text_list)
-# print(X)
-# X_test = ngram_vectorizer.transform(reviews_test_clean)
-
-# X_train, X_val, y_train, y_val = train_test_split(
-#     X, text_sentiment_list, train_size=0.75
-# )
-
-# ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 3), max_features=100, stop_words=basic_stopwords_list)
-# ngram_vectorizer.fit(["I dont feel so good Mr. Strak"])
-# heloo = ngram_vectorizer.transform(["I dont feel so good Mr. Strak"])
 
 # print(X_val)
 # for c in [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]:
